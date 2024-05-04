8c5e7580ffb5df5dff44b9918eba719b2d909ecc
Najiba-Haidari <nhaidari089@gmail.com>
2024-05-04 14:05:04 -0600

    index added

--- models/user.js
+++ models/user.js
@@ -4,15 +4,22 @@ const userSchema = new mongoose.Schema({
     username: {
         type: String,
         required: true,
+        minlength: 3,
+        maxlength: 20,
+        index: true
     },
     email: {
         type: String,
         required: true,
         unique: true,
+        match: [/.+@.+\..+/, 'Must match an email address!'],
+        
     },
     password: {
         type: String,
         required: true,
+        minlength: 6,
+        maxlength: 100
     },
     exercises: [
         {
